ace3deb422742d0ab44734e1941cda6ffb795913
Harshit Sethi <hstsethi@outlook.com>
2024-09-16 23:42:51 -0400

    Create infer-models.py

---
+++ infer-models.py
@@ -0,0 +1,34 @@
+import os
+import time
+from azure.ai.inference import ChatCompletionsClient
+from azure.ai.inference.models import SystemMessage, UserMessage
+from azure.core.credentials import AzureKeyCredential
+
+model_names = ["gpt4-0", "Phi-3.5-mini-instruct", "Mistral-large"]  
+def promptModel(model_name, prompt):
+    endpoint = "https://models.inference.ai.azure.com"
+    model_name = "cohere-command-r-plus"
+    token = os.environ["GITHUB_TOKEN"]
+
+    client = ChatCompletionsClient(
+    endpoint=endpoint,
+    credential=AzureKeyCredential(token),
+)
+
+    response = client.complete(
+    messages=[
+        SystemMessage(content="You are a helpful assistant."),
+        UserMessage(content=prompt)
+    ],
+    model=model_name,
+    temperature=1.0,
+    max_tokens=1000,
+    top_p=1.0
+)
+
+    print(response.choices[0].message.content)
+
+for i in model_names:
+    print("Prompting: " + i)
+    promptModel(i,"What is your name, model version, architecture, and other technical info")
+    time.sleep(5)
